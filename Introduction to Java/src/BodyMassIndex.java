6a617df9b21c3f4454c90e72f78b796825014dce
Nguyễn Văn Hưởng <>
2020-08-05 09:06:45 +0700

    updated

--- Introduction to Java/src/BodyMassIndex.java
+++ Introduction to Java/src/BodyMassIndex.java
@@ -2,21 +2,21 @@ import java.util.Scanner;
 
 public class BodyMassIndex {
     public static void main(String[] args) {
-        double weight, height, ibm;
+        double weight, height, bmi;
         Scanner scanner = new Scanner(System.in);
         System.out.println("Weight: ");
         weight = scanner.nextFloat();
         System.out.println("Height: ");
         height = scanner.nextFloat();
-        ibm = weight/Math.pow(height, 2);
-        if (ibm < 18.5) {
-            System.out.println("The IBM: " + ibm + " ==> " + "Result: Underweight");
-        } else if (ibm < 25) {
-            System.out.println("The IBM: " + ibm + " ==> " + "Result: Normal");
-        } else if (ibm < 30) {
-            System.out.println("The IBM: " + ibm + " ==> " + "Result: Overweight");
+        bmi = weight/Math.pow(height, 2);
+        if (bmi < 18.5) {
+            System.out.println("The IBM: " + bmi + " ==> " + "Result: Underweight");
+        } else if (bmi < 25) {
+            System.out.println("The IBM: " + bmi + " ==> " + "Result: Normal");
+        } else if (bmi < 30) {
+            System.out.println("The IBM: " + bmi + " ==> " + "Result: Overweight");
         } else {
-            System.out.println("The IBM: " + ibm + " ==> " + "Result: Obese");
+            System.out.println("The IBM: " + bmi + " ==> " + "Result: Obese");
         }
     }
 }
