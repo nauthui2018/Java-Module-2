b39cc77d1a54a58159f8d1b358d1d7b6aa662586
Nguyễn Văn Hưởng <>
2020-08-12 16:42:32 +0700

    updated

--- Array and Method in Java/src/CountRepeatingTimeOfLetter.java
+++ Array and Method in Java/src/CountRepeatingTimeOfLetter.java
@@ -7,30 +7,31 @@ public class CountRepeatingTimeOfLetter {
         System.out.println("Enter a string");
         String str = scanner.nextLine();
         int repeat;
-        int i;
+        int i,j,k;
         int count = 0;
         boolean isExist = false;
-        String[] letter = new String[20];
+        String[] letter = new String[str.length()];
+        String temp;
         for (i=0; i<str.length(); i++) {
-            String temp = String.valueOf(str.charAt(i));
-            for (i=0; i<count; i++) {
-                if (letter[i].equals(temp)) {
+            temp = String.valueOf(str.charAt(i));
+            for (j=0; j<count; j++) {
+                if (temp.equals(letter[j])) {
                     isExist = true;
                 }
             }
             if (isExist) {
+                isExist = false;
                 continue;
             } else {
-                letter[i] = temp;
+                letter[j] = temp;
                 count ++;
                 repeat = 1;
-                for (int j=i+1; j<str.length(); j++) {
-                    if (temp.equals(String.valueOf(str.charAt(j)))) {
+                for (k=i+1; k<str.length(); k++) {
+                    if (temp.equals(String.valueOf(str.charAt(k)))) {
                         repeat += 1;
                     }
                 }
                 System.out.println("Letter '" + temp +"' repeat " + repeat + " times");
-                isExist = false;
             }
         }
         System.out.println(Arrays.toString(letter));
