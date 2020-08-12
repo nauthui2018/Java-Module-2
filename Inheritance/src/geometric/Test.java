2b163044aecf60badd6641de7d47cca7454b993f
Nguyễn Văn Hưởng <>
2020-08-12 10:39:07 +0700

    updated

--- Inheritance/src/geometric/Test.java
+++ Inheritance/src/geometric/Test.java
@@ -6,15 +6,19 @@ public class Test {
         Circle circle = new Circle();
         Rectangle rectangle = new Rectangle();
         Square square = new Square();
+        Triangle triangle = new Triangle();
 
-        System.out.println(shape.toString());
-        System.out.println(circle.toString());
-        System.out.println(rectangle.toString());
-        System.out.println(square.toString());
+//        System.out.println(shape.toString());
+//        System.out.println(circle.toString());
+//        System.out.println(rectangle.toString());
+//        System.out.println(square.toString());
+//
+//        square.setSide(5);
+//        System.out.println(square.getSide());
+//        System.out.println(square.getArea());
+//        System.out.println(square.getPerimeter());
 
-        square.setSide(5);
-        System.out.println(square.getSide());
-        System.out.println(square.getArea());
-        System.out.println(square.getPerimeter());
+        System.out.println(triangle.toString());
+
     }
 }
---
+++ Inheritance/src/geometric/Triangle.java
@@ -0,0 +1,68 @@
+package geometric;
+
+import java.util.Arrays;
+
+public class Triangle extends Shape{
+    private double side1 = 1;
+    private double side2 = 1;
+    private double side3 = 1;
+
+    public Triangle() {
+    }
+
+    public Triangle(double side1, double side2, double side3) {
+        this.side1 = side1;
+        this.side2 = side2;
+        this.side3 = side3;
+    }
+
+    public double getSide1() {
+        return side1;
+    }
+
+    public double getSide2() {
+        return side2;
+    }
+
+    public double getSide3() {
+        return side3;
+    }
+
+    public void setSide1(double side1) {
+        this.side1 = side1;
+    }
+
+    public void setSide2(double side2) {
+        this.side2 = side2;
+    }
+
+    public void setSide3(double side3) {
+        this.side3 = side3;
+    }
+
+    public void setSides(double side1, double side2, double side3) {
+        this.side1 = side1;
+        this.side2 = side2;
+        this.side3 = side3;
+    }
+
+    public double[] getSides() {
+        return new double[]{this.side1, this.side2, this.side3};
+    }
+
+    public double getPerimeter() {
+        return this.side1 + this.side2 + this.side3;
+    }
+
+    public double getArea() {
+        double p = this.getPerimeter()/2;
+        return Math.pow(p*(p-this.side1)*(p-this.side2)*(p-this.side3), 1.0/2.0);
+    }
+
+    @Override
+    public String toString() {
+        return "Triangle: " + Arrays.toString(this.getSides()) + "\n" +
+                "Perimeter: " + this.getPerimeter() + "\n" +
+                "Area: " + this.getArea();
+    }
+}
