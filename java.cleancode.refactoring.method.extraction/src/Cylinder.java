1a9871bd68f05e09d0486be90047dc1a2e30e039
Nguyễn Văn Hưởng <>
2020-08-17 17:31:35 +0700

    updated

---
+++ java.cleancode.refactoring.method.extraction/src/Cylinder.java
@@ -0,0 +1,18 @@
+public class Cylinder {
+    public static double getVolume(int radius, int height){
+        double baseArea = getBaseArea(radius);
+        double perimeter = getPerimeter(radius);
+        double volume = perimeter * height + 2 * baseArea;
+        return volume;
+    }
+
+    public static double getPerimeter(int radius) {
+        double perimeter = 2 * Math.PI  * radius;
+        return perimeter;
+    }
+
+    public static double getBaseArea(int radius) {
+        double baseArea = Math.PI * radius * radius;
+        return baseArea;
+    }
+}
