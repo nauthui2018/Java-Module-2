b37eeac62c5e2737662f4f81ab987473d9f684f8
Nguyễn Văn Hưởng <>
2020-08-17 08:30:40 +0700

    updated

--- Abstract Class and Interface/src/comparable/ComparableCircle.java
+++ Abstract Class and Interface/src/comparable/ComparableCircle.java
@@ -16,13 +16,9 @@ public class ComparableCircle extends Circle implements Comparable<ComparableCir
     }
 
     @Override
-    public int compareTo(ComparableCircle n) {
-        if (getRadius() > n.getRadius()) {
-            return 1;
-        } else if (getRadius() < n.getRadius()) {
-            return -1;
-        } else {
-            return 1;
-        }
+    public int compareTo(ComparableCircle o) {
+        if (getRadius() > o.getRadius()) return 1;
+        else if (getRadius() < o.getRadius()) return -1;
+        else return 1;
     }
 }
