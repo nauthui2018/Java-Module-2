7c73943392cae5f5ba07656fad42804867abb1c7
Nguyễn Văn Hưởng <>
2020-08-29 17:53:13 +0700

    updated

--- Data Structures and Algorithms_Stack and Queue/src/reverseArrayByStack.java
+++ Data Structures and Algorithms_Stack and Queue/src/reverseArrayByStack.java
@@ -4,8 +4,13 @@ import java.util.Stack;
 public class reverseArrayByStack {
     public static void main(String[] args) {
         Stack<Integer> myStack = new Stack<>();
+        Stack<String> myStack1 = new Stack<>();
         int[] myArray = new int[5];
+        String[] str = {
+                "getting", "good", "at", "coding",
+                "needs", "a", "lot", "of", "practice"};
         int length = myArray.length;
+        int length1 = str.length;
         for (int i = 0; i < length; i++) {
             myArray[i] = (int) (Math.random() * 20);
             myStack.push(myArray[i]);
@@ -15,5 +20,15 @@ public class reverseArrayByStack {
             myArray[i] = myStack.pop();
         }
         System.out.println(Arrays.toString(myArray));
+
+        for (int i = 0; i < length1; i++) {
+            myStack1.push(str[i]);
+            System.out.print(str[i] + " ");
+        }
+        System.out.println();
+        for (int i = 0; i < length1; i++) {
+            str[i] = myStack1.pop();
+            System.out.print(str[i] + " ");
+        }
     }
 }
