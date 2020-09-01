a180b1eea2a10536fe82574a6235c67011915880
Nguyễn Văn Hưởng <>
2020-09-01 08:37:34 +0700

    updated

---
+++ Java Collection FrameWork/src/ArrangementWithComparableAndComparator/Student.java
@@ -0,0 +1,49 @@
+package ArrangementWithComparableAndComparator;
+
+public class Student implements Comparable<Student>{
+    private String name;
+    private Integer age;
+    private String address;
+
+    public Student(String name, Integer age, String address) {
+        this.name = name;
+        this.age = age;
+        this.address = address;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                '}';
+    }
+
+    @Override
+    public int compareTo(Student student) {
+        return this.getName().compareTo(student.getName());
+    }
+}
